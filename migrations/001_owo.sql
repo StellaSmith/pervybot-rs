de8c1b6007bb78290ce75c2eb0d88e9742c8eb3e
StellaSmith <stellasmith3@outlook.com>
2022-10-15 22:37:28 -0300

    Add sqlx migrations
    Also only use postgres

---
+++ migrations/001_owo.sql
@@ -0,0 +1,8 @@
+CREATE  TABLE "public".pings ( 
+	id                   integer  NOT NULL  ,
+	author_id            integer  NOT NULL  ,
+	channel_id           integer  NOT NULL  ,
+	message_id           integer  NOT NULL  ,
+	"timestamp"          timestamp  NOT NULL  ,
+	CONSTRAINT pk_ping PRIMARY KEY ( id )
+ );
